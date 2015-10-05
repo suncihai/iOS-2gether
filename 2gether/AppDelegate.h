7118b46b035a0fb12d59d922fb2866a5358d93f3
cihai sun <suncihai@gmail.com>
2015-10-05 11:18:37 -0400

    Initial Commit

---
+++ 2gether/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  2gether
+//
+//  Created by suncihai on 15/10/5.
+//  Copyright (c) 2015年 suncihai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 2gether/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  2gether
+//
+//  Created by suncihai on 15/10/5.
+//  Copyright (c) 2015年 suncihai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
